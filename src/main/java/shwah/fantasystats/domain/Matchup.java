8fefb948251bd5e27fa287a83fd13dde5f194ed9
Greg <gaulfield@gmail.com>
2018-11-07 17:14:19 -0500

    g's and s's

--- src/main/java/shwah/fantasystats/domain/Matchup.java
+++ src/main/java/shwah/fantasystats/domain/Matchup.java
@@ -7,5 +7,32 @@ public class Matchup {
 	private int week;
 	private int year;
 	
+	
+	public Player getPlayer1() {
+		return player1;
+	}
+	public void setPlayer1(Player player1) {
+		this.player1 = player1;
+	}
+	public Player getPlayer2() {
+		return player2;
+	}
+	public void setPlayer2(Player player2) {
+		this.player2 = player2;
+	}
+	public int getWeek() {
+		return week;
+	}
+	public void setWeek(int week) {
+		this.week = week;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	
 
 }
--- src/main/java/shwah/fantasystats/domain/Score.java
+++ src/main/java/shwah/fantasystats/domain/Score.java
@@ -3,7 +3,21 @@ package shwah.fantasystats.domain;
 import java.io.Serializable;
 
 public class Score implements Serializable {
+	
 	private double score;
 	private int week;
+	
+	public double getScore() {
+		return score;
+	}
+	public void setScore(double score) {
+		this.score = score;
+	}
+	public int getWeek() {
+		return week;
+	}
+	public void setWeek(int week) {
+		this.week = week;
+	}
 
 }
